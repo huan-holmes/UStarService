f0912cd630def481ccc49566b35766eb426d511b
huan-homles <1124782663@qq.com>
2020-07-21 14:54:08 +0800

    modify start_simulation.sh

--- scripts/start_simulation.sh
+++ scripts/start_simulation.sh
@@ -2,6 +2,6 @@
 source /opt/ros/melodic/setup.bash
 source /home/mingo/UStarService/devel/setup.bash
 ### start gmapping ##############################
-roslaunch ustar_slam ustar_simulation.launch
+roslaunch simulation ustar_simulation.launch
 
 
