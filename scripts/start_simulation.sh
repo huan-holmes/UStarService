ba84f374277b72ac0d407561afc570f8c07f1b4d
huan-holmes <1124782663@qq.com>
2020-09-30 14:53:47 +0800

    Goal: (simulator) change private map. Done: change world_file and map_file; add map.yaml, add map.pgm.

--- scripts/start_simulation.sh
+++ scripts/start_simulation.sh
@@ -5,14 +5,14 @@ move=0
 
 gnome-terminal -x bash -c "roslaunch simulator stage.launch"
 if [ $? -eq 0 ]; then
-	sleep 2
+	sleep 3
 	gnome-terminal --tab -e 'bash -c "roslaunch simulator move.launch"' 
 	move=1
 else
 	echo "stage start falied"
 fi
 if [ $move -eq 1 ]; then
-	sleep 2
+	sleep 15
 	gnome-terminal --tab -e 'bash -c "rosrun rqt_reconfigure rqt_reconfigure"'
 else
 	echo "move start failed"
