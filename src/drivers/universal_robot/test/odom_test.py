7d4c9ab011dc5640824aa9c975ee1a7f969ce5af
huan-holmes <1124782663@qq.com>
2020-11-03 17:12:43 +0800

    Goal: (universal_robot) change odom_test.py. Done: (universal_robot) change odom_test.py.

--- src/drivers/universal_robot/test/odom_test.py
+++ src/drivers/universal_robot/test/odom_test.py
@@ -1,36 +1,57 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 import rospy
+import tf
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
-from queue import Queue
+import csv
+import time
+now_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+file_name = "/home/boocax/UstarService/src/drivers/universal_robot/data/"
+head = ["cmd_vel_x", "cmd_angle", "cmd_pos_x", "cmd_pos_y", "cmd_distance", 
+        "odom_vel_x", "odom_angle", "odom_pos_x", "odom_pos_y", "odom_distance"]
+file_open = open(file_name + now_time + ".csv", "w")
+csv_writer = csv.writer(file_open)
+csv_writer.writerow(head)
+cmd_vel_x_ = 0.0
+cmd_angle_ = 0.0
+cmd_pos_x_ = 0.0
+cmd_pos_y_ = 0.0
+cmd_distance_ = 0.0
 
-cmd_velx_q = Queue()
-cmd_angle_q = Queue()
-odom_velx_q = Queue()
-odom_angle_q = Queue()
-
+odom_vel_x_ = 0.0
+odom_angle_ = 0.0
+odom_pos_x_ = 0.0
+odom_pos_y_ = 0.0
+odom_distance_ = 0.0
 def velocityInfoCallback(msg):
-       rospy.loginfo("Subscribe vel Info: x:%s z:%d", msg.linear.x, msg.angular.z)
-       cmd_velx_q.put(msg.linear.x)
-       cmd_angle_q.put(msg.angular.z) 
-       if (cmd_velx_q.qsize() >= 10): 
-                cmd_velx_q.get()
-                cmd_angle_q.get()
+        global cmd_vel_x_
+        rospy.loginfo("Subscribe vel Info: x:%s z:%d", msg.linear.x, msg.angular.z)
+        cmd_vel_x_ = msg.linear.x
+       
 
 def odomInfoCallback(msg):
+        global cmd_vel_x_, cmd_angle_, cmd_pos_x_, cmd_pos_y_, cmd_distance_ 
+        global odom_vel_x_, odom_angle_, odom_pos_x_, odom_pos_y_, odom_distance_
         rospy.loginfo("Subscribe vel Info: x:%s  y:%d  z:%d", msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z)
-        odom_velx_q.put(msg.twist.twist.linear.x)
-        odom_angle_q.put(msg.twist.twist.angular)
-        if (odom_velx_q.qsize() >= 10): 
-                odom_velx_q.get()
-                odom_angle_q.get()
+        (r, p, y) = tf2.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+        odom_angle_ = y
+        odom_vel_x_ = msg.twist.twist.linear.x
+        odom_pos_x_ = msg.pose.pose.position.x
+        odom_pos_y_ = msg.pose.pose.position.y
+        
+        csv_writer.writerow([cmd_vel_x_, cmd_angle_, cmd_pos_x_, cmd_pos_y_, cmd_distance_,
+                         odom_vel_x_, odom_angle_, odom_pos_x_, odom_pos_y_, odom_distance_])
+                
+
 
 def main():
         odom_node = rospy.init_node("odom_test_node", anonymous=True)
         odom_info_sub = rospy.Subscriber("/odom", Odometry, odomInfoCallback)
         vel_info_sub = rospy.Subscriber("/cmd_test_topic", Twist, velocityInfoCallback)
         rospy.spin()
+        file_open.close()
+
 if __name__ == '__main__':
     main()
