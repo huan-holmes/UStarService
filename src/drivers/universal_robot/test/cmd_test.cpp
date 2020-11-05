c923e42557790a604a0d3c570ef1c14307d3e8f2
huan-holmes <1124782663@qq.com>
2020-11-05 16:00:26 +0800

    Goal: (universal_robot) test odom. Done: (universal_robot) test odom.

--- src/drivers/universal_robot/test/cmd_test.cpp
+++ src/drivers/universal_robot/test/cmd_test.cpp
@@ -116,9 +116,9 @@ void PublishVelocity::velocityInfoPublisher()
         return;
     }
     distance_ = sqrt(pow(transform.getOrigin().x(), 2) + pow(transform.getOrigin().y(), 2));
-    cmd_vel_.linear.x = 0.0;
+    cmd_vel_.linear.x = 0.2;
     cmd_vel_.linear.y = 0.0;
-    cmd_vel_.angular.z = -0.4; 
+    cmd_vel_.angular.z = 0.0; 
     // if (fabs(distance_ - dest_distance_) < tolerance_distance_) 
     // {
     //     ROS_INFO_STREAM(fabs(distance_ - dest_distance_));
