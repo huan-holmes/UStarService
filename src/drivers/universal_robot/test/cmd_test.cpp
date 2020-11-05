6f447706879f39019c71a0b5b90922c17c940fa3
huan-holmes <1124782663@qq.com>
2020-11-05 14:56:07 +0800

    Goal: (universal_robot) test odom. Done: (universal_robot) test odom.

--- src/drivers/universal_robot/test/cmd_test.cpp
+++ src/drivers/universal_robot/test/cmd_test.cpp
@@ -128,7 +128,7 @@ void PublishVelocity::velocityInfoPublisher()
     } 
     else
     {
-        cmd_vel_.linear.x = 0.0;
+        cmd_vel_.linear.x = vel_x_;
         cmd_vel_.linear.y = 0.0;
         cmd_vel_.angular.z = 0.0; 
     }
