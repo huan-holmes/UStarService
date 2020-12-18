0850d28a23728fe9b841d571783d3a6ad7035726
huan-holmes <1124782663@qq.com>
2020-12-18 11:48:49 +0800

    Goal: socket. Done: socket.

--- src/perception/src/test.cpp
+++ src/perception/src/test.cpp
@@ -20,7 +20,7 @@
 #include <signal.h>
 #define PORT 1234
 #define BUFFER_SIZE 1500
-static int sockfd;
+
 static int client_fd;
 
 using namespace cv;
@@ -29,13 +29,14 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_socket");
     ROS_INFO("-----------------test");
+    int sockfd;
     struct sockaddr_in serv_addr, client;
     //创建socket 
     bzero(&serv_addr,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(PORT);
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
     {
         perror("Socket failed!\n");
@@ -45,43 +46,44 @@ int main(int argc, char **argv)
         perror("bind serv_addr ");
         exit(1);
     }
-    sockaddr clntAddr;
-    listen(sockfd, 20);
-
+    if (listen(sockfd, 10) == -1)
+    {
+        perror("listen::error");
+    }
+    else
+    {
+        printf("listening:...");
+    }
+    
+    socklen_t sin_size = sizeof(struct sockaddr_in);
+    if((sockfd=accept(sockfd,(struct sockaddr *)&client,&sin_size))==-1)
+    {
+        perror("accept:");
+        return -1;
+    }
+    else
+    {
+        printf("accept successful!\r\n");
+    }
+    int recLen = 0;
    // Mat image = cv::imread(argv[1],CV_LOAD_IMAGE_COLOR);
     char buffer[BUFFER_SIZE] = {0};
-    while (1)
+    while(1)
     {
-        ROS_INFO("working!");
-        //int addr_len = sizeof(client);
-        //int clntSock = accept(sockfd, (sockaddr*)&clntAddr, &addr_len);
-        socklen_t client_len;
-        client_len=sizeof(client_fd);
-        int recvRe = recvfrom(sockfd, buffer,BUFFER_SIZE, 0, (struct sockaddr*)&client_fd,&client_len);
-        //ROS_INFO_STREAM(clntAddr.sa_data);
-        // if (image.empty())
-        //     continue;
-        //int clntSock = accept(sockfd, (sockaddr*)&clntAddr, &nSize);
-        //int ret = recv(sockfd,buffer,BUFFER_SIZE,0);
-        //send(sockfd, buffer, ret, 0); //将数据原样返回
-        // if(ret == -1){
-        //    perror("recvfrom error: ");
-        //    exit(1);
-        // }
-        // if (ret == -1)
-        //     continue;
-        // std::cout << buffer <<endl;
-        // vector<uchar> data_encode;
-        // vector<int> quality = vector<int>(2);
-        // quality[0] = CV_IMWRITE_JPEG_QUALITY;
-        // quality[1] = 50;
-        // imencode(".jpg", image, data_encode, quality); //将图像编码
-        // //char encodeImg[655350];
-        // int nSize = data_encode.size();
-        // std::cout << "图像的数据： " << nSize << endl;
-        // // write(sockfd, reinterpret_cast<uchar*>(&nSize), sizeof(int));
-        // sendto(sockfd, &data_encode[0], nSize, 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    }
+        memset(buffer,0,sizeof(buffer));
+        if((recLen = recv(sockfd,buffer,BUFFER_SIZE,0))==-1)
+        {
+            perror("recv:");
+        }
+        else
+        {
+            if(recLen>0)
+            {
+                recLen = 0;
+                printf("Receive a message:%s\r\n",buffer);
+            }
+        }
+    }    
     close(sockfd);
     ROS_INFO("No error.");
     return 0;
